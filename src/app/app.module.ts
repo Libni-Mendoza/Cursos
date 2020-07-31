347dbda5e6db8dbad11429d4284d6a33bc565ff8
Libni-Mendoza <libni.heldai@gmail.com>
2020-07-30 21:38:22 -0500

    Avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PonentesComponent } from './ponentes/ponentes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarruselComponent,
+    PrincipalComponent,
+    NavbarComponent,
+    FooterComponent,
+    PonentesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ponentes/ponentes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ponentes',
+  templateUrl: './ponentes.component.html',
+  styleUrls: ['./ponentes.component.css']
+})
+export class PonentesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
